88cc7609feb2241b7418a6726e5b2112aa8bbd81
Inquisitorone <57298818+Inquisitorone@users.noreply.github.com>
2025-06-17 11:46:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,76 @@
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+import os
 
-API_TOKEN = "8061299708:AAGNWggA1u1nxosvOimH2k53FeQ--aXcReE"
+API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
-@dp.message_handler(commands=["start"])
-async def send_welcome(message: types.Message):
-    await message.reply("Привет! Я Telegram-бот и я работаю 😊")
+class Form(StatesGroup):
+    city = State()
+    vin = State()
+    dlink = State()
+    language = State()
+    comment = State()
+    confirm = State()
 
-if __name__ == "__main__":
+@dp.message_handler(commands='start')
+async def start_cmd(message: types.Message):
+    await message.answer("Введите город:")
+    await Form.city.set()
+
+@dp.message_handler(state=Form.city)
+async def process_city(message: types.Message, state: FSMContext):
+    await state.update_data(city=message.text)
+    await message.answer("Введите VIN:")
+    await Form.vin.set()
+
+@dp.message_handler(state=Form.vin)
+async def process_vin(message: types.Message, state: FSMContext):
+    await state.update_data(vin=message.text)
+    await message.answer("Введите версию Dlink:")
+    await Form.dlink.set()
+
+@dp.message_handler(state=Form.dlink)
+async def process_dlink(message: types.Message, state: FSMContext):
+    await state.update_data(dlink=message.text)
+    await message.answer("Введите язык (UA или RU):")
+    await Form.language.set()
+
+@dp.message_handler(state=Form.language)
+async def process_language(message: types.Message, state: FSMContext):
+    await state.update_data(language=message.text)
+    await message.answer("Введите комментарий (или '-' если нет):")
+    await Form.comment.set()
+
+@dp.message_handler(state=Form.comment)
+async def process_comment(message: types.Message, state: FSMContext):
+    await state.update_data(comment=message.text)
+    data = await state.get_data()
+    summary = f"""
+📋 <b>Заявка:</b>
+Город: {data['city']}
+VIN: {data['vin']}
+Версия Dlink: {data['dlink']}
+Язык: {data['language']}
+Комментарий: {data['comment']}
+"""
+    await message.answer(summary, parse_mode="HTML")
+    await message.answer("Подтвердите заявку? (да/нет)")
+    await Form.confirm.set()
+
+@dp.message_handler(state=Form.confirm)
+async def process_confirm(message: types.Message, state: FSMContext):
+    if message.text.lower() == "да":
+        await message.answer("Заявка подтверждена. Спасибо!")
+    else:
+        await message.answer("Заявка отменена.")
+    await state.finish()
+
+if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
